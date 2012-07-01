42b30069d73da0df2b56828c8a1f373b76f796d8
wasMitNetzen <fgodard@gmail.com>
2012-07-01 18:07:27 +0200

    WIkibot now uses the same config file as the bash part of pirem

--- var/www/pirem/config.php
+++ var/www/pirem/config.php
@@ -1,17 +1,27 @@
 <?php
+//if (php_sapi_name()!=="cli") die("CLI-access only");
+
 //grab class
-include('wikibot.php5');
+include('wikibot.php');
+// Config File
+$configFile = "/etc/pirem/pirem.conf";
+// Workaround for non-root installation
+if (!is_file($configFile)) $configFile = "../../../etc/pirem/pirem.conf";
+
+if (!is_file($configFile)) die("Config File not found.");
+$settings = parse_ini_file($configFile);
+
 //get password
-$username = "USERNAME"; //A registered username
-$password = "PASSWORD";
+$username = $settings["WIKIUSERNAME"];
+$password = $settings["WIKIUSERPASSWORD"];
 
-$wikiurl = "http://de.wikipedia.org/wiki/";
-$artikelurl = "http://de.wikipedia.org/";
+$wikiurl = $settings["WIKIURL"];
+$artikelurl = $settings["WIKIARTICLEURL"];
 
-$vorlage["datum"] = "Vorlage:Datum";
-$vorlage["zeit"] = "Vorlage:Uhrzeit";
-$vorlage["ort"] = "Vorlage:Ort";
+$vorlage["datum"] = $settings["WIKIDATETEMPLATE"];
+$vorlage["zeit"] =  $settings["WIKITIMETEMPLATE"];
+$vorlage["ort"] = $settings["WIKIPLACETEMPLATE"];
 
-$adminmail = "admin@example.com";
+$adminmail = $settings["WIKIDEBUGADMINMAIL"];
 
-$botpassword = "MyPasswordIsLongerThanYours";
+$botpassword = $settings["WIKIBOTPASSWORD"];
